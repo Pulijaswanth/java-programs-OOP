d01d620b7da47c4695f10ff436dd7637642de1cc
Pulijaswanth <128456193+Pulijaswanth@users.noreply.github.com>
2023-04-20 19:45:43 +0530

    Add files via upload
    To demonstrate the use of throws

---
+++ TestThrows.java
@@ -0,0 +1,29 @@
+//use of throws
+class TestThrows
+{
+	void f1() throws ArrayIndexOutOfBoundsException
+	{
+		int res[]={100};
+		System.out.println("Result is "+res[1]);
+	}
+	void f2()
+	{
+		f1();
+	}
+	void f3()
+	{
+		f2();
+	}
+	public static void mian(String args [])
+	{
+		TestThrows ob=new TestThrows();
+		try
+		{
+			ob.f3();
+		}
+		catch(ArrayIndexOutOfBoundsException obj)
+		{
+			System.out.println("out of bound ");
+		}
+	}
+}
